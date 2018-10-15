3455d4a57b7f3ea9981dc33c1f43697fe183f2cd
Andrew Fellenz <andrewfellenz@gmail.com>
2018-10-14 22:33:01 -0400

    Added starting comments

---
+++ guessing_game.py
@@ -0,0 +1,14 @@
+# Start of file
+
+# Function to pick a random int
+
+
+# Variable for top score
+
+# Welcome message!
+
+# loop for question - check for higher or lower and tell player
+
+# end game and display guesses
+
+# Ask user if they would like to play again
