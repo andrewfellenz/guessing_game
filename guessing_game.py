3b8a92ebf1cb419dee4870e336a5718c184085d8
Andrew Fellenz <andrewfellenz@gmail.com>
2018-10-15 01:01:21 -0400

    Removed the play_game function to help test game.

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,4 @@
+
 # Start of file
 from random import randint
 
@@ -18,7 +19,7 @@ def player_guess(guess_number):
         try:
             guess = int(input("Guess #{}> ".format(guess_number)))
         except ValueError:
-            print("That's not a number")
+            print("That's not a number.")
         else:
             if guess < 1 or guess > 100:
                 print("You can only guess numbers between 1 and 100.")
@@ -30,26 +31,24 @@ def player_guess(guess_number):
 print("Welcome to Drew's Guessing Game. The object of the game is to guess the correct number in the fewest guesses possible. Good luck!\n")
 
 
-def play_game()
-  while True:
-      guess = player_guess(guess_number)
-      if guess == number_to_guess:
-          if guess_number < high_score or high_score == False:
-              print("{} is the correct number! Your score of {} guesses is the new high score!".format(guess, guess_number))
-              high_score = guess_number
-              break
-          else:
-              print("{} is the correct number and you guessed it in {} guesses!".format(guess, guess_number))
-              
-              break
-      elif guess > number_to_guess:
-          print("That number is too high.")
-          guess_number += 1
-      elif guess < number_to_guess:
-          print("That number is too low.")
-          guess_number += 1
+while True:
+    guess = player_guess(guess_number)
+    if guess == number_to_guess:
+        if guess_number < high_score or high_score == False:
+            print("{} is the correct number! Your score of {} guesses is the new high score!".format(guess, guess_number))
+            high_score = guess_number
+            break
+        else:
+            print("{} is the correct number and you guessed it in {} guesses!".format(guess, guess_number))       
+            break
+    elif guess > number_to_guess:
+        print("That number is too high.")
+        guess_number += 1
+    elif guess < number_to_guess:
+        print("That number is too low.")
+        guess_number += 1
 
-play_game()
+
 
 
 
