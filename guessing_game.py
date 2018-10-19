6880251b1120fe88c4031f10779e06db90f08d65
Andrew Fellenz <andrewfellenz@gmail.com>
2018-10-18 22:15:02 -0400

    Added all pep8 adjustments recommend by pep8online.com/checkresult

--- guessing_game.py
+++ guessing_game.py
@@ -13,7 +13,6 @@ def player_guess(guess_number):
                 continue
             else:
                 return guess
-                break
 
 
 def game_start():
@@ -27,25 +26,38 @@ def play_game(best_score):
     number_to_guess = randint(1, 100)
     guess_number = 1
     if best_score:
-        print("\nThe current best score is {}.\nSee if you can beat that.\nGuess a number between 1 and 100.\n".format(best_score))
+        print("\nThe current best score is {}."
+              "\nSee if you can beat that."
+              "\nGuess a number between 1 and 100."
+              "\n".format(best_score)
+              )
     else:
-        print("\nWelcome to Drew's Number Guessing Game.\nType a number between 1 and 100 to guess.\nThe fewer tries you take the better your score!\n")
+        print("\nWelcome to Drew's Number Guessing Game."
+              "\nType a number between 1 and 100 to guess."
+              "\nThe fewer tries you take the better your score!\n"
+              )
     while True:
         guess = player_guess(guess_number)
         if guess == number_to_guess:
-            if guess_number < best_score or best_score == False:
-                print("\n{} is the correct number!\nThat's the best you've done!\n{} is your new best score!".format(guess, guess_number))
+            if guess_number < best_score or best_score is False:
+                print("\n{} is the correct number!"
+                      "\nThat's the best you've done!"
+                      "\n{} is your new best score!"
+                      .format(guess, guess_number)
+                      )
                 best_score = guess_number
                 break
             else:
-                print("{} is the correct number.\nYou got it in {} guesses!".format(guess, guess_number))
+                print("{} is the correct number.\n"
+                      "You got it in {} guesses!".format(guess, guess_number)
+                      )
                 break
         elif guess > number_to_guess:
             print("That number is too high.")
             guess_number += 1
         elif guess < number_to_guess:
-           print("That number is too low.")
-           guess_number += 1
+            print("That number is too low.")
+            guess_number += 1
 
     while True:
         try:
